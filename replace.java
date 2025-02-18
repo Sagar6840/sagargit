750cf0a677f173fc9a78f7239d0e5a8f3169fd55
Sagar6840 <sagarp.23.becs@acharya.ac.in>
2025-02-19 00:00:04 +0530

    write a java program for a reverse the number of given number

--- replace.java
+++ replace.java
@@ -1,15 +1,14 @@
 
 import java.util.Scanner;
+
 public class replace 
 {
     public static void main(String[] args)
     {
-        System.out.println( "type  a string whatever you want"); 
-     Scanner SC= new Scanner(System.in);
-     String sagar=SC.nextLine();
-     String remove = sagar.replace(" ","");
-     System.out.println(remove);
+        System.out.println("type  a string whatever you want");
+        Scanner SC = new Scanner(System.in);
+        String sagar = SC.nextLine();
+        String remove = sagar.replace(" ", "");
+        System.out.println(remove);
     }
-
-    
 }
