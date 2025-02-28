f41c123c7796cc20e89c7c2020ceae4eef9daf0d
Sagar6840 <sagarp.23.becs@acharya.ac.in>
2025-02-28 18:46:45 +0530

    Delete day1.java

--- day1.java
+++
@@ -1,9 +0,0 @@
-public class day1
-{
-public static void main(String[] args) {
-    String input ="hello  world";
-    String output = input.replace( "  ","%20" );
-System.out.println(output);
-
-}
-}
