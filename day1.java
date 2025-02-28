d75ea5e064ca8df8d9181e75f53c39dd5b225146
Sagar6840 <sagarp.23.becs@acharya.ac.in>
2025-02-28 17:54:12 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -3,7 +3,7 @@ public class day1
 public static void main(String[] args) {
     String input ="hello  world";
     String output = input.replace( "  ","%20" );
-System.out.println(output)
+System.out.println(output);
 
 }
 }
