20ce1b3855bc4230b05a6e718b3096cf4fd557b5
Sagar6840 <sagarp.23.becs@acharya.ac.in>
2025-02-28 17:39:09 +0530

    GIVEN string replace all spaces with 20%.

---
+++ day1.java
@@ -0,0 +1,14 @@
+public class day1
+{
+public static void main(String[] args) {
+    String input ="hello  world";
+    String output = input.replace( "  ","%20" );
+System.out.println(output);
+}
+    static void replace(String input)
+    {
+        input="helloword";
+System.out.println(" ");
+
+}
+}
